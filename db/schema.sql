e4aedb321fb04e93d343ceee9cc43a12d761bfc8
IvanCastillo1986 <total180training@gmail.com>
2023-09-28 01:30:56 -0400

    added Pokemon table and seed files for original 151 pokemon

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,21 @@ CREATE TABLE decks (
     pokemon_id INT
 );
 
+CREATE TABLE pokemon (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    hp INT,
+    atk INT,
+    def INT,
+    special_atk INT,
+    special_def INT,
+    speed INT,
+    type1 VARCHAR,
+    type2 VARCHAR,
+    move1 VARCHAR,
+    move2 VARCHAR,
+    exp INT,
+    lvl INT,
+    front_img TEXT,
+    rear_img TEXT
+);
